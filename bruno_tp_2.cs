436d6a61bfab442fe9d99c0187eb755c161e0ded
brunoapaez20-bit <brunoapaez20@gmail.com>
2026-03-19 12:02:38 -0300

    Add files via upload

---
+++ bruno_tp_2.cs
@@ -0,0 +1,275 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("25 > 30");
+
+if (25 > 30)
+
+{
+
+    Console.WriteLine("verdadero");
+
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+
+Console.WriteLine("14 <= 14");
+
+if (14 <= 14)
+
+{
+    Console.WriteLine("verdadero");
+
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+
+Console.WriteLine("0 == 0 ");
+
+if (0 == 0)
+
+{
+    Console.WriteLine("verdadero");
+
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+
+Console.WriteLine("10 != 10");
+
+if (10 != 10)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+
+Console.WriteLine("8 >= 3");
+
+if (8 >= 3)
+
+{
+    Console.WriteLine("verdadero");
+
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("15 < 15");
+
+if (15 < 15)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+
+Console.WriteLine("100 != 50");
+
+if (100 != 50)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("7 == 7.0");
+
+if (7 == 7.0)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+
+Console.WriteLine("0.5 > 0 ");
+
+if (0.5 > 0)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+
+Console.WriteLine("-5 < -2");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+
+
+
+
+
+
+
+Console.WriteLine("(5 > 3) AND (10 < 20)");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("(8 == 10) OR (5 != 2)");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("NOT (10 == 10)");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("(15 > 10) AND NOT (5 > 10)");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("(10 + 2 > 10) OR (5 * 2 < 8)");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+
+Console.WriteLine("Si edad = 16: NOT (edad >= 18)");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("(10 // 3 == 3) AND (10 % 3 == 1)");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("Si nombre = \"Ana\": (nombre == \"Juan\") OR (nombre == \"Ana\")");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("((5 * 2) + 10 > 15) AND (20 / 4 == 5)");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("");
+
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+
+Console.WriteLine("Si temperatura = 35 y Humedad = Falso: (temperatura > 30) AND (humedad == Verdadero)");
+
+int temperatura = 35;
+bool humedad = false;
+if (-5 < -2)
+
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
