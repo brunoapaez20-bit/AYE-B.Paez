03b4e64ec72b051bef07930b4673983c32909932
brunoapaez20-bit <brunoapaez20@gmail.com>
2026-03-26 11:54:20 -0300

    Add files via upload

---
+++ tp_3.cs
@@ -0,0 +1,29 @@
+﻿ string ejercicio1(int n)
+{
+    string resultado;
+    if (n > 0)
+        resultado = "es positivo";
+    else if (n <  18)
+        resultado = "es negativo";
+    else
+        resultado = "0";
+    return resultado;
+}
+
+Console.WriteLine(ejercicio1(Convert.ToInt32(Console.ReadLine())));
+
+
+string ejercicio2(int n)
+{
+    string resultado;
+    if (n > 0
+ )
+        resultado = "bienvenido a la fiesta";
+    else if (n < 0)
+        resultado = "losiento eres muy joven";
+    else
+        resultado = "bievienido a la fiesta";
+    return resultado;
+}
+
+Console.WriteLine(ejercicio2(Convert.ToInt32(Console.ReadLine())));
