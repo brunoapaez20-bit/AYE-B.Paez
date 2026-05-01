4b9257cce9167bd0244391adbda5163d6bb99d4d
brunoapaez20-bit <brunoapaez20@gmail.com>
2026-05-01 19:04:53 -0300

    Add exercises for password check and even/odd evaluation

--- tp_3.cs
+++ tp_3.cs
@@ -27,3 +27,46 @@ string ejercicio2(int n)
 }
 
 Console.WriteLine(ejercicio2(Convert.ToInt32(Console.ReadLine())));
+
+Console.WriteLine("Ejercicio 3");
+Console.Write("Ingresa la contraseña: ");
+string password = Console.ReadLine();
+
+if (password == "python123")
+{
+    Console.WriteLine("¡Contraseña correcta! Acceso concedido.");
+}
+else
+{
+    Console.WriteLine("¡Contraseña incorrecta, Autodestrucción en 5 minutos!");
+}
+
+Console.WriteLine("Ejercicio 4: Par o Impar?");
+Console.Write("Ingresa un número entero: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("El número es par.");
+}
+else
+{
+    Console.WriteLine("El número es impar");
+}
+
+Console.WriteLine("Ejercicio 5: Entrada gratis?");
+Console.Write("¿Cuántos años tienes?: ");
+int edad = int.Parse(Console.ReadLine());
+
+Console.Write("¿Compraste palomitas? (si/no): ");
+string palomitas = Console.ReadLine().ToLower();
+
+if (edad > 65 && palomitas == "si")
+{
+    Console.WriteLine("¡Felicidades! Tienes entrada gratuita al cine.");
+}
+else
+{
+    Console.WriteLine("Compra la entrada o raja de acá");
+}
+
