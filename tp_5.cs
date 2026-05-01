f197aa8c36e8e6677f427b7c374e74e9016d177e
brunoapaez20-bit <brunoapaez20@gmail.com>
2026-05-01 19:59:32 -0300

    Add files via upload

---
+++ tp_5.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Ejercicio 1");
+string passSecreta = "profe123";
+string intento = "";
+
+while (intento != passSecreta)
+{
+    Console.Write("Ingresa la contraseña: ");
+    intento = Console.ReadLine();
+
+    if (intento != passSecreta)
+    {
+        Console.WriteLine("Inténtalo de nuevo");
+    }
+}
+
+Console.WriteLine("Entrando");
+
+Console.WriteLine("Ejercicio 2");
+
+for (int i = 5; i >= 1; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("¡Listo para despegar!");
+Console.WriteLine("¡Despegue!");
+
+Console.WriteLine("Ejercicio 3");
+int numeroSecreto = 7;
+int adivinanza = 0;
+
+while (adivinanza != numeroSecreto)
+{
+    Console.Write("Adivina el número secreto: ");
+    adivinanza = Convert.ToInt32(Console.ReadLine());
+
+    if (adivinanza == numeroSecreto)
+    {
+        Console.WriteLine("¡Felicidades! Adivinaste el número.");
+    }
+}
+
+Console.WriteLine("Ejercicio 4");
+int sumaTotal = 0;
+int numeroIngresado = -1; 
+
+while (numeroIngresado != 0)
+{
+    Console.Write("Ingresa un número (0 para terminar): ");
+    numeroIngresado = Convert.ToInt32(Console.ReadLine());
+
+    sumaTotal = sumaTotal + numeroIngresado;
+}
+
+Console.WriteLine("La suma total es: " + sumaTotal);
