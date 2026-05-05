c80ed30f7e0183355bb4cc2276e0edfae4ef42de
brunoapaez20-bit <brunoapaez20@gmail.com>
2026-05-05 11:34:30 -0300

    Add files via upload

---
+++ tp_4.cs
@@ -0,0 +1,35 @@
+﻿for (int X = 1; X <= 10; X++)
+{
+    Console.WriteLine(X);
+}
+
+
+string texto = "¡Hola mundo!";
+int repeticiones = 5;
+
+for (int i = 0; i < repeticiones; i++)
+{
+    Console.WriteLine(texto);
+}
+
+
+for (int X = 2; X <= 20; X += 2)
+{
+    Console.WriteLine(X);
+}
+
+
+for (int X = 7; X <= 70; X += 7)
+{
+    Console.WriteLine(X);
+}
+
+
+int acumulador = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    acumulador += i;
+
+    Console.WriteLine(acumulador);
+}
